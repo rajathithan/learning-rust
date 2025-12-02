8ae454b029e3b536372f2b8ae22205e61bd7393d
Rajathithan Rajasekar <rajathithan@gmail.com>
2025-12-02 22:27:02 +0530

    variables

---
+++ variables_test/src/main.rs
@@ -0,0 +1,9 @@
+
+
+fn main() {
+    let apples : i32 = 5;
+    let bananas : i32 = 10;
+    let total_fruits : i32 = apples + bananas;
+    println!("I have {1} apples & {0} bananas", bananas, apples);
+    println!("apples {} & bananas {} add to {} fruits", apples, bananas, total_fruits); 
+}
